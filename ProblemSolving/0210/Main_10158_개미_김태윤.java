14718fee7f16ed45d70e26480054b4e0b5ea15f9
taeyun Kim <kimty0301@gmail.com>
2022-02-17 00:00:52 +0900

    0217_baekjoon

---
+++ ProblemSolving/0210/Main_10158_개미_김태윤.java
@@ -0,0 +1,66 @@
+package algo_ex;
+
+import java.util.Scanner;
+
+public class Main_10158_개미_김태윤 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt(); //6
+		int h = sc.nextInt(); //4
+		
+		int p = sc.nextInt(); //4
+		int q = sc.nextInt(); //1
+		
+		int t = sc.nextInt(); //8
+		
+		p += t;
+		q += t;
+		
+		p%=(w*2);
+		q%=(h*2);
+		
+		if(p<=w && q<=h) { //3사분면(오리지널)
+			
+		}else if(p<=w && q>h) { //2사분면
+			q = h-(q-h);
+		}else if(p>w && q<=h) { //4사분면
+			p = w-(p-w);
+		}else { //1사분면
+			q = h-(q-h);
+			p = w-(p-w);
+		}
+		
+		
+//		int dx = 1;
+//		int dy = 1;
+//		
+//		for(int T = 0; T<t; T++) {
+//			int dp = p+dx;
+//			int dq = q+dy;
+//			
+//			if(dp==w) {
+//				dx = -1;
+//			}else if(dp==0) {
+//				dx = 1;
+//			}
+//			
+//			
+//
+//			if(dq==h) {
+//				dy = -1;
+//			}else if(dq==0) {
+//				dy = 1;
+//			}
+//			
+//			p = dp;
+//			q = dq;
+//
+////			System.out.println(p+" "+q);
+//		}
+		
+		System.out.println(p+" "+q);
+	}
+
+}
